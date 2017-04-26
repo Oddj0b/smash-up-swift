3482d2c7aa19e2a87d5781b645d39ff216d1206f
Villars Gimm <villars.gimm@gmail.com>
2017-04-26 12:01:33 +0200

    Initial Commit

---
+++ Smash-Up/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smash-Up
+//
+//  Created by Villars Gimm on 26/04/2017.
+//  Copyright © 2017 RibEye Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
