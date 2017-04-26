c9ec4cbddddb22a6598f500d7fdab12c9d28b7ac
Villars Gimm <villars.gimm@gmail.com>
2017-04-26 12:14:52 +0200

    Initial Commit

---
+++ Smash-Up/Faction.swift
@@ -0,0 +1,12 @@
+//
+//  Faction.swift
+//  Smash-Up
+//
+//  Created by Villars Gimm on 26/04/2017.
+//  Copyright © 2017 RibEye Studios. All rights reserved.
+//
+
+import Foundation
+class Faction{
+    var name: String?
+}
---
+++ Smash-Up/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  Smash-Up
+//
+//  Created by Villars Gimm on 26/04/2017.
+//  Copyright © 2017 RibEye Studios. All rights reserved.
+//
+
+import Foundation
+class Player {
+    var name: String?
+    var factions: [String]?
+    
+}
