e8de5aeb4a5d23dab496ef9acf19b48104bf0919
Villars Gimm <villars.gimm@gmail.com>
2017-05-14 19:06:27 +0200

    CollectionView controller

---
+++ Smash-Up/GameSetupViewController.swift
@@ -0,0 +1,47 @@
+//
+//  GameSetupViewController.swift
+//  Smash-Up
+//
+//  Created by Villars Gimm on 26/04/2017.
+//  Copyright © 2017 RibEye Studios. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "PlayerCell"
+
+class GameSetupViewController: UICollectionViewController {
+//    @IBOutlet weak var cellLabel: UILabel!
+    @IBOutlet weak var cellPlayerName: UITextField!
+//    @IBOutlet weak var cellFaction2: UIButton!
+//    @IBOutlet weak var cellFaction1: UIButton!
+    let Players = ["Danielle", "Villars", "Peter", "Flemming"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(Players.count)
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 90, height: 90)
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionView!.dataSource = self
+        collectionView!.delegate = self
+        collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView!.backgroundColor = UIColor.white
+        self.view.addSubview(collectionView!)
+    }
+    
+    // MARK: UICollectionViewDataSource
+override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return Players.count
+    }
+
+
+override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = UIColor.black
+        return cell
+    }
+}
