3eb1a899253be152e7b3c5fb6a067e0bb2c996b9
Villars Gimm <villars.gimm@gmail.com>
2017-12-09 21:30:10 +0100

    Stackview working

--- Smash-Up/PlayerView.swift
+++ Smash-Up/PlayerView.swift
@@ -8,18 +8,11 @@
 
 import Foundation
 import UIKit
-class PlayerView: UIView{
-    var playerName: UILabel = UILabel()
-    var faction1: UIButton = UIButton()
-    var faction2: UIButton = UIButton()
-    var playerFrame: CGRect = CGRect(x: 0, y: 0, width: 200, height: 95)
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+class PlayerView: UIViewController{
+    override func viewDidLoad() {
+        var playerName: UILabel = UILabel()
+        var faction1: UIButton = UIButton()
+        var faction2: UIButton = UIButton()
+        var playerFrame: CGRect = CGRect(x: 0, y: 0, width: 200, height: 95)
     }
 }
--- Smash-Up/StackViewController.swift
+++ Smash-Up/StackViewController.swift
@@ -9,38 +9,24 @@
 import UIKit
 
 class StackViewController: UIViewController {
-    
-    @IBOutlet weak var adjustPlayers: UIStepper!
     @IBOutlet weak var playerList: UIStackView!
-    @IBOutlet weak var howManyPlayers: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        adjustPlayers.minimumValue = 2.0
-        adjustPlayers.maximumValue = 4.0
-        let currentNumberOfPlayers = adjustPlayers.value
-        howManyPlayers.text = "\(currentNumberOfPlayers) players"
+        
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func adjustPlayerClick(_ sender: Any) {
-        let currentNumberOfPlayers = adjustPlayers.value
-        howManyPlayers.text = "\(currentNumberOfPlayers) players"
-        let rect = CGRect(x: 0, y: 0, width: 200, height: 95)
-        let playerView = PlayerView.init(frame: rect)
-        playerView.backgroundColor = .yellow
-        addPlayer(player: playerView)
-        NSLog("Adding player", playerView)
-        
-    }
-    func addPlayer(player: UIView){
-        playerList.addArrangedSubview(player)
+    @IBAction func addPlayer(_ sender: Any) {
+        print("Presseing")
+        let imageView = UIImageView.init(image: #imageLiteral(resourceName: "smash-up"))
+        playerList.addArrangedSubview(imageView)
         NSLog("Arranged subviews", playerList.arrangedSubviews)
     }
 }
