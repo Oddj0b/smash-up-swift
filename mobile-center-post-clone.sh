0b625dba6f6b76d0540fd399391d37659f1f3970
Villars Gimm <villars.gimm@gmail.com>
2017-12-09 22:42:33 +0100

    Remove unused scripts

--- mobile-center-post-clone.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-git clone git@github.com:airbnb/lottie-ios.git
-
-fastlane --version
--- mobile-center-pre-build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-curl -O https://bootstrap.pypa.io/get-pip.py
-sudo python get-pip.py
-
-pip --version
-
